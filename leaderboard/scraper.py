bf62072d256809b94fc9560681ddca5a41b16fb7
Alex Wice <alexwice@gmail.com>
2020-08-24 19:20:23 +1000

    through to weekly 203

--- leaderboard/scraper.py
+++ leaderboard/scraper.py
@@ -36,7 +36,7 @@ for d in range(63, 139):
     contests.append("weekly-contest-" + str(d))
 
 d0 = 139
-for d in range(1, 33):
+for d in range(1, 34):
     contests.append("biweekly-contest-" + str(d))
     contests.append("weekly-contest-" + str(d0))
     d0 += 1
